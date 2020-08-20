6f168b9b7af35a16786592b07c67e76cdba3a416
Tarcisio Valentim <33500059+TarcisioValentim@users.noreply.github.com>
2020-08-20 00:27:16 -0300

    key

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,15 +5,7 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyDu4AysvGeMOgndOGx20eT726Lulf7oKvc",
-    authDomain: "todolistapp-e0736.firebaseapp.com",
-    databaseURL: "https://todolistapp-e0736.firebaseio.com",
-    projectId: "todolistapp-e0736",
-    storageBucket: "todolistapp-e0736.appspot.com",
-    messagingSenderId: "668103338301",
-    appId: "1:668103338301:web:2ec6dc1b425e90bf63bd54",
-    measurementId: "G-8XSP2ZN5X5"
-  }
+    apiKey: "sua key"
 };
 
 /*
